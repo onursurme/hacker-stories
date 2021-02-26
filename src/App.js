97b1178945091131427c4181c669adf86061692c
drx <onursurme@yahoo.com>
2021-02-26 14:19:51 +0300

    yazdığımız fonksiyonu jsx içinde çağırdık

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
+function getTitle(title){
+  return title;
 }
 
 function App() {
   return (
     <div>
       <h1>
-        {welcome.greeting} {welcome.title} !
+        Hello {getTitle('React!')}
       </h1>
 
       <label htmlFor="search">Search: </label>
