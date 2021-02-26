165fa80e4908917942744e3c51be8bad347c1060
drx <onursurme@yahoo.com>
2021-02-26 16:11:49 +0300

    list map yaptık

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-function getTitle(title){
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramox, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+]
 
 function App() {
   return (
     <div>
       <h1>
-        Hello {getTitle('React!')}
+        My Hacker Stories
       </h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
-    </div>
+      <hr />
+      {list.map(function(item) {
+        return <div>{item.title}</div>;
+      })}
+       </div>
   );
 }
 
