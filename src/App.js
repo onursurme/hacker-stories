a60687de10ef520e0b4bcd40d4b00cf34d2753b0
drx <onursurme@yahoo.com>
2021-02-27 11:56:06 +0300

    list map ile yaptığımızı örnekte küçük bir değişiklik yaptık

--- src/App.js
+++ src/App.js
@@ -27,10 +27,25 @@ function App() {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
       <hr />
-      {list.map(function(item) {
-        return <div>{item.title}</div>;
+      {list.map(function (item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>
+              {item.author}
+            </span>
+            <span>
+              {item.num_comments}
+            </span>
+            <span>
+              {item.points}
+            </span>
+          </div>
+        );
       })}
-       </div>
+    </div>
   );
 }
 
